59594202f46214c31d16a333ac99c262354369e8
User200925 <dalimovhojiakbar@gmail.com>
2024-11-08 19:14:06 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// Запрашиваем информацию у пользователя
+let firstName = prompt("Введите ваше имя:");
+let lastName = prompt("Введите вашу фамилию:");
+let age = prompt("Введите ваш возраст:");
+let userLocation = prompt("Где вы живете:"); // Изменено на userLocation
+let favoriteSubject = prompt("Ваш любимый предмет:");
+
+// Выводим информацию в консоль
+console.log("Имя: " + firstName);
+console.log("Фамилия: " + lastName);
+console.log("Возраст: " + age);
+console.log("Место жительства: " + userLocation); // Изменено на userLocation
+console.log("Любимый предмет: " + favoriteSubject);
